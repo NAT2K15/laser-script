7fc2bad67bc1b32d81ed838379f20c970771a2cc
NAT2K15 <72643615+NAT2K15@users.noreply.github.com>
2021-06-23 16:41:31 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,39 +1,34 @@
 local enabled = false
+local timer_opti = 1000
 
 RegisterCommand('+taser_toggle', function()
 	local ped = PlayerPedId()
-	if GetSelectedPedWeapon(ped) == GetHashKey("WEAPON_STUNGUN") then
 		if enabled then
 			enabled = false
 			displaytext("Laser has been ~r~Disabled")
+			timer_opti = 1000
 		else
 			displaytext("Laser has been ~g~Enabled")
 			Citizen.CreateThread(function()
 				enabled = true
 				while enabled do 
-					Citizen.Wait(0)
+					timer_opti = 1000
 					local camview = GetFollowPedCamViewMode()
 					local crouch = GetPedStealthMovement(ped)
 					if IsPlayerFreeAiming(PlayerId()) then
-						if GetSelectedPedWeapon(ped) == GetHashKey("WEAPON_STUNGUN") then
-							local weapon = GetSelectedPedWeapon(ped)
-							local playerloc = GetPedBoneCoords(ped, 0x6F06)
-							local offset = GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 105.0, 0.0)
-							local hit, coords, value3 = RayCastPed(playerloc, 15000, ped)
-							if hit ~= 0 then
-								if camview == 4 and crouch == 1 then
-									DrawLine(playerloc.x, playerloc.y, playerloc.z -0.30, coords.x, coords.y, coords.z, config.LaserColorR, config.LaserColorG, config.LaserColorB, config.LaserColorA)
-								else 
-									DrawLine(playerloc.x, playerloc.y, playerloc.z, coords.x, coords.y, coords.z, config.LaserColorR, config.LaserColorG, config.LaserColorB, config.LaserColorA)
-								end
-								DrawSphere2(coords, 0.01, config.LaserColorR, config.LaserColorG, config.LaserColorB, config.LaserColorA)
-							end
+						timer_opti = 0
+						local weapon = GetCurrentPedWeaponEntityIndex(ped)
+						local offset = GetOffsetFromEntityInWorldCoords(weapon, 0, 0, -0.01)
+						local hit, coords = RayCastPed(offset, 15000, ped)
+						if hit ~= 0 then
+							DrawLine(offset.x, offset.y, offset.z, coords.x, coords.y, coords.z, config.LaserColorR, config.LaserColorG, config.LaserColorB, config.LaserColorA)
+							DrawSphere2(coords, 0.01, config.LaserColorR, config.LaserColorG, config.LaserColorB, config.LaserColorA)
 						end
 					end
+					Citizen.Wait(timer_opti)
 				end
 			end)
 		end
-	end
 end, false)
 
 RegisterCommand('-taser_toggle', function() end, false)
@@ -66,7 +61,7 @@ function RayCastPed(pos,distance,ped)
 	}
 
 	local a, b, c, d, e = GetShapeTestResult(StartShapeTestRay(pos.x, pos.y, pos.z, destination.x, destination.y, destination.z, -1, ped, 1))
-    return b, c, e
+    return b, c
 end
 
 
@@ -78,4 +73,4 @@ end
 
 function DrawSphere2(pos, radius, r, g, b, a)
 	DrawMarker(28, pos.x, pos.y, pos.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, radius, radius, radius, r, g, b, a, false, false, 2, nil, nil, false)
-  end
+end
